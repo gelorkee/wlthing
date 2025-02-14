678b5631eddd4902950ed3a68a24d007e652da26
gelorkee <157400985+gelorkee@users.noreply.github.com>
2025-02-14 10:33:05 -0300

    Update wl.lua

--- wl.lua
+++ wl.lua
@@ -2,7 +2,9 @@ local allowed = {
   1564439460,
   53785569,
   2783000531,
-  7486983397
+  7486983397,
+  7033305781,
+  5216022
 }
 
 if not table.find(allowed, game.Players.LocalPlayer.UserId) then 
